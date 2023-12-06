4ba1c523322ab0c874f23d78d30bd47df58fa302
Alex Voievudko <alex.voievudko@gmail.com>
2023-12-06 17:16:11 +0100

    feat(immfly-app): implement cart store

---
+++ apps/immfly-app/src/store/cart.ts
@@ -0,0 +1,64 @@
+import { create } from 'zustand';
+import { Product } from '../types';
+
+export interface CartItem extends Product {
+  quantity: number;
+}
+
+type CartStore = {
+  cart: CartItem[];
+  discount: number;
+  totalCount: () => number;
+  totalPrice: () => number;
+  actions: {
+    addProduct: (product: Product) => void;
+    removeProduct: (product: Product) => void;
+    clearCart: () => void;
+    setDiscount: (discount: number) => void;
+    clearDiscount: () => void;
+  };
+};
+
+const useCartStore = create<CartStore>((set, get) => ({
+  cart: [],
+  discount: 0,
+  totalCount: () => get().cart.reduce((acc, item) => acc + item.quantity, 0),
+  totalPrice: () => {
+    const { discount } = get();
+    const total = get().cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
+    const totalDiscount = total * discount;
+    return total - totalDiscount;
+  },
+  actions: {
+    addProduct: product => {
+      const { cart } = get();
+      const index = cart.findIndex(item => item.id === product.id);
+      if (index === -1) {
+        set({ cart: [...cart, { ...product, quantity: 1 }] });
+      } else {
+        cart[index].quantity += 1;
+        set({ cart: [...cart] });
+      }
+    },
+    removeProduct: product => {
+      const { cart } = get();
+      const index = cart.findIndex(item => item.id === product.id);
+      if (index !== -1) {
+        cart[index].quantity -= 1;
+        if (cart[index].quantity === 0) {
+          cart.splice(index, 1);
+        }
+        set({ cart: [...cart] });
+      }
+    },
+    clearCart: () => set({ cart: [] }),
+    setDiscount: discount => set({ discount }),
+    clearDiscount: () => set({ discount: 0 }),
+  },
+}));
+
+export const useCart = () => useCartStore(store => store.cart);
+export const useCartDiscount = () => useCartStore(store => store.discount);
+export const useCartTotalCount = () => useCartStore(store => store.totalCount());
+export const useCartTotalPrice = () => useCartStore(store => store.totalPrice());
+export const useCartActions = () => useCartStore(store => store.actions);
